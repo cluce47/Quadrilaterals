2ef35d84faec4529193f367a5546216f16d6880b
Carol <Carol@192.168.2.25>
2021-05-01 18:43:26 -0400

    creating subclass and doing calculations

---
+++ src/quadrilaterals/Parallelogram.java
@@ -0,0 +1,13 @@
+package quadrilaterals;
+/**
+ *
+ * @author Carol
+ */
+public class Parallelogram extends Quadrilaterals{
+    double height;
+    
+    public Parallelogram(){
+        height=0.0;
+    }
+    
+}
